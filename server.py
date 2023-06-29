c5784df16779ca730ba038f85748607c5a617f3c
leolukaz123 <122065825+leolukaz123@users.noreply.github.com>
2023-06-29 20:46:27 +1000

    added the base docs for the main app

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, send_file, jsonify
 import os
 
 app = Flask(__name__)
@@ -8,7 +8,24 @@ def hello():
     name = request.args.get('name', 'World')
     return 'Hello! you requested ' + ' with name ' + name
 
+@app.route("/logo.png", methods=['GET'])
+def plugin_logo():
+    filename = 'logo.png'
+    return send_file(filename, mimetype='image/png')
+
+@app.route("/.well-known/ai-plugin.json", methods=['GET'])
+def plugin_manifest():
+    with open("./.well-known/ai-plugin.json") as f:
+        text = f.read()
+        return jsonify(text)
+
+@app.route("/openapi.yaml", methods=['GET'])
+def openapi_spec():
+    with open("openapi.yaml") as f:
+        text = f.read()
+        return jsonify(text)
+
 if __name__ == "__main__":
     port = int(os.getenv('PORT', 80))
-    print('Listening on port %s' % (port))
+    print(f'Listening on port {port}')
     app.run(host='0.0.0.0', port=port)
